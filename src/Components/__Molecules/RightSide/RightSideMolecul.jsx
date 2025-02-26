6eb62eb5c67b160830d6e6d7b451c7ecc6c028f6
lashameskhidze <lashameskhidze0@gmail.com>
2025-02-26 14:50:05 -0800

    egari

--- src/Components/__Molecules/RightSide/RightSideMolecul.jsx
+++ src/Components/__Molecules/RightSide/RightSideMolecul.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./RightSideMolecul.css";
 import RightSideTexts from "../../__Atoms/RightSideTexts.JSX";
-// import RightSideTexts from "../../__Atoms/RightSideTexts";
-// import RightSideTexts from "../../__Atoms/RightSideTexts";
 
 const RightSideMolecul = () => {
   return (
