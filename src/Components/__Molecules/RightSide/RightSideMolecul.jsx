a08aa46a49e804bcdf2610a43c26fa2d4a2b822b
lashameskhidze <lashameskhidze0@gmail.com>
2025-02-26 14:42:37 -0800

    end

---
+++ src/Components/__Molecules/RightSide/RightSideMolecul.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./RightSideMolecul.css";
+import RightSideTexts from "../../__Atoms/RightSideTexts.JSX";
+// import RightSideTexts from "../../__Atoms/RightSideTexts";
+// import RightSideTexts from "../../__Atoms/RightSideTexts";
+
+const RightSideMolecul = () => {
+  return (
+    <div className="RightSideMoleculDiv">
+      <RightSideTexts />
+    </div>
+  );
+};
+
+export default RightSideMolecul;
